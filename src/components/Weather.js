19aeb08cacf111e131d1f41c03556bdc17aa7d2d
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2021-01-20 15:45:18 +0530

    display details

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,19 +1,33 @@
 import React, { useEffect, useState } from "react";
 
 const Weather = () => {
+
+    const [city, setCity] = useState("")
+    const [country, setCountry] = useState("")
+    const [temperature, setTemperature] = useState("")
+    const [icon, setIcon] = useState("")
+    const [weatherCondition, setWeatherCondition] = useState("")
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (pos) => {
         let lat = pos.coords.latitude;
         let lon = pos.coords.longitude;
-        const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b86a040eae9280afdfff416815b2f168`;
+        //const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b86a040eae9280afdfff416815b2f168`;
         console.log("Latituide is: ", pos.coords.latitude);
         console.log("Longitude is: ", pos.coords.longitude);
         fetch(
           `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b86a040eae9280afdfff416815b2f168`
         )
           .then((res) => res.json())
-          .then((res)=>console.log(res));
+          .then((res)=>{
+              console.log(res)
+              setCity(res.name)
+              setCountry(res.sys.country)
+              setTemperature(res.main.temp - 273.15)
+              setIcon(res.weather[0].icon)
+              setWeatherCondition(res.weather[0].description)
+          });
       },
       (error) => {
         console.error("Error Code = " + error.code + " - " + error.message);
@@ -25,10 +39,12 @@ const Weather = () => {
   }, []);
 
   return (
-    <div>
-      <h1>
-        This is where the current weather of your location will be displayed
-      </h1>
+    <div className="Weather">
+      <p className="Weather-city-name">City: {city}</p>
+      <p className="Weather-country-name">Country: {country}</p>
+      <p className="Weather-temperature">Current temperature: {temperature}</p>
+      <p className="weather-description">Weather Condition: {weatherCondition}</p>
+      <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" className="icon"/>
     </div>
   );
 };
