d21d794605394c5e7749091a7e65f2bb66d28756
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2021-01-20 14:21:02 +0530

    initial commit

---
+++ src/components/Weather.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react'
+
+const Weather = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Weather
---
+++ src/components/fetch.js
@@ -0,0 +1,3 @@
+import axios from 'axios'
+
+const API_KEY = "b86a040eae9280afdfff416815b2f168"
