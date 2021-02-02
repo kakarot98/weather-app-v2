f3edf635e2d08bd1bb341e03ee0f9a8722151864
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2021-02-02 17:03:39 +0530

    minor changes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { fetchUserLocation, fetchWeather } from "./fetch";
+import styles from './Weather.module.css'
 
 const Weather = () => {
-  const [weatherData, setWeatherData] = useState({});
+ // const [weatherData, setWeatherData] = useState({});
   const [city, setCity] = useState("");
   const [country, setCountry] = useState("");
   const [temperature, setTemperature] = useState("");
@@ -17,11 +18,10 @@ const Weather = () => {
         const data = await fetchWeather(location.latitude, location.longitude);
 
         if (data !== null) {
-          setWeatherData(data)
+          //setWeatherData(data)
           setCity(data.name);
           setCountry(data.sys.country);
           let decimalTemperature = data.main.temp - 273.15;
-
           setTemperature(decimalTemperature.toFixed(1));
           setWeatherCondition(data.weather[0].main);
           setIcon(data.weather[0].icon);
@@ -36,21 +36,22 @@ const Weather = () => {
   }, []);
 
   return (
-    <div className="weather-box">
-      {temperature && (<p className="Weather-temperature">
-        {temperature}
-        {"\u00b0"} C, {weatherCondition}
-      </p>)}
-      {icon && (
-        <img
+    <div className={styles.weatherBox}>
+      {!temperature && <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="loading"/>}
+      {temperature && (<div className={styles.weatherDescritpion}>
+        <p className={styles.weatherDescriptionData}>
+        {temperature + "\u00b0C, "}
+        {weatherCondition}
+      </p>
+      <img
           src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
           alt=""
-          className="icon"
+          className={styles.weatherDescriptionIcon}
         />
-      )}
-      <p className="Weather-location">
-        {city}, {country}
+         <p className={styles.weatherDescriptionLocation}>
+        {city + ", " + country}
       </p>
+      </div>)}
     </div>
   );
 };
